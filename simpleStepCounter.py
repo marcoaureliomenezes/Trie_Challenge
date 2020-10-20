5ca5032f0b13d59510e86bfb842ec5bacb18c75f
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-10-20 10:37:57 -0300

    simpleStepCounter to read column steps from CSV file

--- simpleStepCounter.py
+++ simpleStepCounter.py
@@ -7,3 +7,11 @@ def simpleStepCounter(vet):
     counter += count
     return counter
 
+def simpleStepCounterCSV(loadedData):
+    lastRow = 0; counter = 0
+    for index, row in loadedData.iterrows():
+        if row[1] < lastRow:
+            counter += lastRow
+        lastRow = row[1]
+    counter += row[1]
+    return counter
