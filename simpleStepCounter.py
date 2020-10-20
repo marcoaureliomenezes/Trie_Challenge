97c34f8462ede3402270d25fa299162e86ff6817
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-10-20 03:41:46 -0300

    Creating simple tests

--- simpleStepCounter.py
+++ simpleStepCounter.py
@@ -1,7 +1,3 @@
-import numpy as np
-
-vet = [1, 2, 3, 10, 0, 1, 7, 10, 20, 10, 0, 40, 20, 3, 0, 10, 1000];
-
 def simpleStepCounter(vet):
     lastRow = 0; counter = 0
     for count in vet:
---
+++ simpleStepCounterTests.py
@@ -0,0 +1,5 @@
+import simpleStepCounter as simple
+vet = [1, 2, 3, 1, 1, 2, 0, 0, 0, 1, 0, 2, 1000];
+result = 3 + 2 + 1 + 1000
+
+print("Valor Esperado: " + str(result) + " - Resultado: " + str(simple.simpleStepCounter(vet)))
