0dde83a124e420560c45a52f4d52d6be033a25d5
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-10-21 11:37:04 -0300

    validatedWalking method implementation. There's yet error. The condition for a walking end must be implemente

---
+++ walkingNumbers.py
@@ -0,0 +1,26 @@
+import simpleStepCounter as ssc
+
+def stepVel(vet1, vet2):
+        deltaStep = vet2[1] - vet1[1]
+        deltaTime = vet2[0] - vet1[0]
+        return deltaStep / deltaTime
+
+def validatedWalking(data) :
+        stack = []
+        trainStack = [0]
+        walkingQuantity = 0; 
+        walkingTime = 0
+        for it in data:
+                instant = it[0]
+                trainStack.append(it[1])
+                stepNumber = ssc.simpleStepCounter(trainStack)
+                stack.append([instant + 60, stepNumber])
+                for dadoGuardado in stack:
+                        if instant >= dadoGuardado[0]:
+                                if (stepNumber - dadoGuardado[1]) >=120 :
+                                        walkingQuantity += 1
+                                        walkingTime += 1
+                                        stack = []
+                                        print(walkingQuantity)
+                                        
+        return [walkingQuantity, walkingTime]
