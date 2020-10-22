3df54307397795cf6eff21465fcdaf96a91c6bdf
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-10-22 17:01:14 -0300

    File that manage timestamps date received and related tests implemented

---
+++ testTimestampsConversion.py
@@ -0,0 +1,26 @@
+example1 = "2020-03-01 22:44:00.047143516"
+example2 = "2020-03-01 22:44:01.047143516"
+
+example3 = "2020-02-28 22:45:01.047143516"
+example4 = "2020-02-28 22:46:01.047143516"
+
+example5 = "2020-03-01 22:44:00.047143516"
+example6 = "2020-03-01 23:44:00.047143516"
+
+example7 = "2020-03-01 22:44:00.047143516"
+example8 = "2020-03-02 22:44:00.047143516"
+
+
+example9 = "2020-01-01 07:25:41.047143516"
+example10 = "2020-02-01 07:25:41.047143516"
+
+example11 = "2020-04-01 07:25:41.047143516"
+example12 = "2020-05-01 07:25:41.047143516"
+
+
+print(subtractTime(example1, example2))
+print(subtractTime(example3, example4))
+print(subtractTime(example5, example6))
+print(subtractTime(example7, example8))
+print(subtractTime(example9, example10))
+print(subtractTime(example11, example12))
--- timestampsHandler.py
+++ timestampsHandler.py
@@ -1,7 +1,3 @@
-example1 = "2020-03-01 00:00:00.047143516"
-example2 = "2020-02-28 23:59:59.047143525"
-timePattern = [60, 60, 1]
-
 def leapYear(year):
     if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
         return True
@@ -15,7 +11,6 @@ def breakTimestamps(timestamps):
 def dayToSecond(day):
     return day * 60 * 60 * 24
 
-
 def monthToSecond(month, year):
     seconds = 0
     months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
@@ -26,57 +21,39 @@ def monthToSecond(month, year):
         if i == month:
             break
     return seconds
-print(monthToSecond(3, 2020))
-print(monthToSecond(2, 2020))
+
+def yearToSecond(year):
+    seconds = 0.0
+    for i in range(1, int(year)):
+        if leapYear(i):
+            seconds += dayToSecond(366)
+        else:
+            seconds += dayToSecond(365)
+    return seconds
+
+
 
 def breakDate(date) :
-    months = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
     dateBroken = date.split("-")
-    
     day = float(dateBroken[2]) - 1
     month = float(dateBroken[1]) - 1
     year = float(dateBroken[0])
-    
-    dayToSeconds = day * 60 * 60 * 24
-    counter = 0
-    monthDays = 0
-    for i in months:
-        monthDays += i
-        if(counter == month):
-            break
-        counter += 1
-        if leapYear(year):
-            pass
-    monthToSeconds = float(month) * (60 * 60 * 24 * months[month])
-    yearToSeconds = float(dateBroken[0] - 1970) * (60 * 60 * 24)
-    timeSeconds = dayToSeconds + monthToSeconds + yearToSeconds
-    return timeSeconds
+
+    seconds = dayToSecond(day) + monthToSecond(month,year) + yearToSecond(year) 
+    return seconds
 
 def breakTime(time) :
     timeBroken = time.split(":")
     seconds = float(timeBroken[2])
     minutesToSeconds = float(timeBroken[1]) * 60
-    hoursToSeconds = float(timeBroken[0]) * 60
+    hoursToSeconds = float(timeBroken[0]) * 3600
     timeSeconds = seconds + minutesToSeconds + hoursToSeconds
     return timeSeconds
 
 
 def convertTimeToSeconds(time): 
     timestampsBroken = breakTimestamps(time)
-    print(timestampsBroken)
-    date = breakDate(timestampsBroken[0])
-    print(date)
-    time = breakTime(timestampsBroken[1])
-    print(time)
-
-    # timeInSeconds = float(time[2]) * 1 + float(time[1]) * 60 + float(time[0]) * 3600
-    # return timeInSeconds
+    return breakDate(timestampsBroken[0]) + breakTime(timestampsBroken[1])
 
 def subtractTime(time1, time2):
     return convertTimeToSeconds(time2) - convertTimeToSeconds(time1)
-
-
-
-# print(convertTimeToSeconds(example1))
-# print(convertTimeToSeconds(example2))
-# ano: 1000000000 * 60 * 60 * 24
