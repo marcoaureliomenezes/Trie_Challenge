5daf4bb40644bc8b712b3433ed46e5b970d02a94
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-10-24 20:56:15 -0300

    Changing the paradigm. Step Absolut filter implemented

--- main.py
+++ main.py
@@ -1,6 +1,26 @@
-from src.StepCounter import StepCounter
+import pandas as pd
+from datetime import datetime
 
-stepCounter = StepCounter("data/data.csv")
+data = pd.read_csv('data.csv', sep= ',', parse_dates=['timestamp'])
+
+# This line filters data taking out repeated numbers if the a 2 times
+# consecutives.
+df = data.loc[data["count"].diff(2) != 0]
 
 
-print("Assignment 1 - Total steps:", stepCounter.stepCounterDeclarative())
+# This block generates the absolute step counter (step number is
+# never reset) through the following steps
+
+# a column with difference between the element and its previous one.
+# It returns the negative numbers that stores how many steps we had
+# before the reset
+df["diff"] = df["count"].diff()
+# To get only the step number at the reset moment we filter the
+# diff column to contain just zero on all steps besides the reset moments 
+df["reset_event"] = df["diff"].apply(lambda x : x if x < 0 else 0)
+# With this step we creates a column with the reset time step numbers
+df["reset_steps_accumulator"] = df["reset_event"].cumsum() * (-1)
+
+# The table absoluteSteps is made through the the sum of the
+# count column and reset_steps_accumulator
+df["absoluteSteps"] = df["reset_steps_accumulator"] + df["count"]
--- todo-list.md
+++
@@ -1,5 +0,0 @@
-# List of todos
-
-
-#   Create a method that recognizes if a walking end
-#   Solve the validatedWalking method to pass in tests
