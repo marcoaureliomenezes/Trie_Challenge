c6f2b1a9c84e96697dad190d68257c421171356c
Marco Menezes <marcoaurelioreislima@gmail.com>
2020-10-26 11:18:24 -0300

    readme, method to transform timestamp column to second column and method  to get absolute steps column implemented

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pandas as pd
 from datetime import datetime
 
-data = pd.read_csv('data.csv', sep= ',', parse_dates=['timestamp'])
+data = pd.read_csv('data-test4.csv', sep= ',', parse_dates=['timestamp'])
 
 # This line filters data taking out repeated numbers if the a 2 times
 # consecutives.
@@ -10,17 +10,28 @@ df = data.loc[data["count"].diff(2) != 0]
 
 # This block generates the absolute step counter (step number is
 # never reset) through the following steps
-
 # a column with difference between the element and its previous one.
 # It returns the negative numbers that stores how many steps we had
 # before the reset
-df["diff"] = df["count"].diff()
-# To get only the step number at the reset moment we filter the
-# diff column to contain just zero on all steps besides the reset moments 
-df["reset_event"] = df["diff"].apply(lambda x : x if x < 0 else 0)
-# With this step we creates a column with the reset time step numbers
-df["reset_steps_accumulator"] = df["reset_event"].cumsum() * (-1)
+def absoluteSteps():
+    delta_count = df["count"].diff() 
+    reset_event = delta_count.apply(lambda x : x if x < 0 else 0)
+    reset_step_accumulator = reset_event.cumsum() * (-1)
+    absolute_steps = reset_step_accumulator + df["count"]
+    return absolute_steps
 
-# The table absoluteSteps is made through the the sum of the
-# count column and reset_steps_accumulator
-df["absoluteSteps"] = df["reset_steps_accumulator"] + df["count"]
+def totalSteps():
+    return absoluteSteps().max()
+
+def timestampToSeconds():
+    timeSeconds = (df["timestamp"] - df["timestamp"].loc[0]).dt.total_seconds()
+    return timeSeconds
+stepNumber = totalSteps()
+
+df["timestamp"] = timestampToSeconds()
+df["Absolute steps"] = absoluteSteps()
+df["velocity"] = df["count"].diff() / (df["timestamp"].diff())
+
+# next block
+
+print(df)
---
+++ readme.md
@@ -0,0 +1,16 @@
+Contador de passos
+------------------
+
+Seguem os dados recebidos de um contador de passos remoto. 
+Quando a rede está disponível, o contador evia timestamp e o número de passos acumulados. 
+Se a pessoa apertar botão reset, a contagem de passos começa do 0. 
+
+Utilizando as ferramentas como numpy e pandas, descobrir:
+
+* Quantos passos no total foram registrados pelo contador;
+
+* Quanto tempo no total a pessoa esteve caminhando, e quantas caminhadas a 
+  pessoa fez, tendo em vista que durante a caminhada a pessoa realiza pelo 
+  menos 1200 passos no período de 10 minutos.
+
+
